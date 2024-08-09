7a0dbded3e55f4fe2c45e159d29dabe75c5730ee
Glaymar A de França <glaymar2010@live.com>
2024-08-09 05:08:27 -0300

    :sparkles: feat: Add "Metricas" à Página de Visualização de Empresas

--- empresarios/views.py
+++ empresarios/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Documento, Empresas
+from .models import Documento, Empresas, Metricas
 from django.contrib import messages
 from django.contrib.messages import constants
 
@@ -108,3 +108,19 @@ def excluir_dc(request, id):
     documento.delete()
     messages.add_message(request, constants.SUCCESS, "Documento excluído com sucesso")
     return redirect(f'/empresarios/empresa/{documento.empresa.id}')
+
+
+def add_metrica(request, id):
+    empresa = Empresas.objects.get(id=id)
+    titulo = request.POST.get('titulo')
+    valor = request.POST.get('valor')
+    
+    metrica = Metricas(
+        empresa=empresa,
+        titulo=titulo,
+        valor=valor
+    )
+    metrica.save()
+
+    messages.add_message(request, constants.SUCCESS, "Métrica cadastrada com sucesso")
+    return redirect(f'/empresarios/empresa/{empresa.id}')
