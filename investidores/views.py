dd14f5f57d058e474ec2e68d13de06e5dc1362be
Glaymar A de França <glaymar2010@live.com>
2024-08-10 07:49:43 +0000

    :sparkles: feat: Página de Proposta de investimento

--- investidores/views.py
+++ investidores/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
-
+from django.contrib import messages
+from django.contrib.messages import constants
 from empresarios.models import Empresas, Documento
+from investidores.models import PropostaInvestimento
 
 def sugestao(request):
     if not request.user.is_authenticated:
@@ -32,3 +34,38 @@ def ver_empresa(request, id):
     empresa = Empresas.objects.get(id=id)
     documentos =  Documento.objects.filter(empresa=empresa)
     return render(request, 'ver_empresa.html', {'empresa': empresa, 'documentos': documentos}) 
+
+
+def realizar_proposta(request, id):
+    valor = request.POST.get('valor')
+    percentual = request.POST.get('percentual')
+    empresa = Empresas.objects.get(id=id)
+
+    propostas_aceitas = PropostaInvestimento.objects.filter(empresa=empresa).filter(status='PA')
+    total = 0
+
+    for pa in propostas_aceitas:
+        total = total + pa.percentual
+
+    if total + float(percentual)  > empresa.percentual_equity:
+        messages.add_message(request, constants.WARNING, 'O percentual solicitado ultrapassa o percentual máximo.')
+        return redirect(f'/investidores/ver_empresa/{id}')
+
+
+    valuation = (100 * int(valor)) / int(percentual)
+
+    if valuation < (int(empresa.valuation) / 2):
+        messages.add_message(request, constants.WARNING, f'Seu valuation proposto foi R${valuation} e deve ser no mínimo R${empresa.valuation/2}')
+        return redirect(f'/investidores/ver_empresa/{id}')
+
+    pi = PropostaInvestimento(
+        valor = valor,
+        percentual = percentual,
+        empresa = empresa,
+        investidor = request.user,
+    )
+
+    pi.save()
+
+    #messages.add_message(request, constants.SUCCESS, f'Proposta enviada com sucesso')
+    return redirect(f'/investidores/assinar_contrato/{pi.id}')
